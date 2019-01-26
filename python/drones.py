2de87919fc58da37e93966cc604bb0f69a6123f5
dastronmighty <eoghan.hogan@ucdconnect.ie>
2019-01-26 10:17:27 +0000

    map with circles

---
+++ python/drones.py
@@ -0,0 +1,15 @@
+import math
+
+F = 5  # time penalty caused by load
+
+
+def dronesRadius(T, S):
+    climbSpeed = 0.3 / (S/60)
+    return (((T - (4*climbSpeed) - (T * .05)) * (S / 60)) / 2)
+
+
+drones = [[40, 79], [35, 64], [18, 60], [15, 60], [24, 79], [16, 64]]
+
+for i in range(len(drones)):
+    print("drone "+str(i + 1)+" can travel up to " +
+          str(dronesRadius(drones[i][0], drones[i][1]))+"km to a medical center")
