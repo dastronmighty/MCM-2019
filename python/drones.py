275df7a70e9d785ddba55c88113a5ca8f20b9016
dastronmighty <eoghan.hogan@ucdconnect.ie>
2019-01-27 16:43:15 +0000

    temp pic

--- python/drones.py
+++ python/drones.py
@@ -3,13 +3,22 @@ import math
 F = 5  # time penalty caused by load
 
 
-def dronesRadius(T, S):
-    climbSpeed = 0.3 / (S/60)
-    return (((T - (4*climbSpeed) - (T * .05)) * (S / 60)) / 2)
+# def dronesRadius(T, S):
+#     climbSpeed = 0.3 / (S/60)
+#     return (((T - (4*climbSpeed) - (T * .05)) * (S / 60)) / 2)
 
 
-drones = [[40, 79], [35, 64], [18, 60], [15, 60], [24, 79], [16, 64]]
+# drones = [[40, 79], [35, 64], [18, 60], [15, 60], [24, 79], [16, 64]]
 
-for i in range(len(drones)):
-    print("drone "+str(i + 1)+" can travel up to " +
-          str(dronesRadius(drones[i][0], drones[i][1]))+"km to a medical center")
+# for i in range(len(drones)):
+#     print("drone "+str(i + 1)+" can travel up to " +
+#           str(dronesRadius(drones[i][0], drones[i][1]))+"km to a medical center")
+
+
+#Container L, W, H
+Container = [200, 20, 20]
+
+#Smaller Box(s) L, W, H
+Boxes = [20, 20, 20]
+
+def
